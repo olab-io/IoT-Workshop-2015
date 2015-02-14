147fcc203919b577cf5d334b3fa2c49dc98f6fd4
Christopher Baker <me@christopherbaker.net>
2015-02-14 14:25:12 -0600

    Serial pass through arduino.

---
+++ Arduino/SerialPassThrough/SerialPassThrough.ino
@@ -0,0 +1,25 @@
+// Pass data from ESP chip to 
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  if (Serial.available())
+    mySerial.write(Serial.read());
+}
+
